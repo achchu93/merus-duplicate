32609e60a4d9667555bdce4929d1bd0cc8bb5d95
achchu93 <achchu.zats@gmail.com>
2021-06-12 01:11:32 +0530

    added: header stylings

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,83 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import tw, { theme, styled, css } from "twin.macro"
+
+const styles = {
+	header: () => [
+		css`
+			background-color: ${theme`colors.primaryGrey`};
+			color: ${theme`colors.home`};
+		`,
+		tw`w-full h-12 fixed inset-x-0 top-0 z-40 lg:h-12`,
+	],
+	nav: () => [
+		tw`grid grid-cols-12 gap-x-1 relative px-2 h-full items-center lg:gap-x-4 lg:px-10`,
+	],
+}
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+	<header
+		css={[
+			css`
+				background-color: ${theme`colors.primaryGrey`};
+				color: ${theme`colors.home`};
+			`,
+			tw`w-full h-12 fixed inset-x-0 top-0 z-40 lg:h-20`,
+		]}
+	>
+		<nav
+			css={[
+				tw`grid grid-cols-12 gap-x-1 relative px-2 h-full items-center lg:gap-x-4 lg:px-10`,
+			]}
+		>
+			<div css={[tw`col-span-12 relative flex items-center`]}>
+				<div css={[tw`relative flex items-start`]}>
+					<button
+						css={[
+							css`
+								backface-visibility: hidden;
+								opacity: 0;
+								transform: rotate(-180deg);
+								animation-delay: 1s;
+							`,
+							tw`lg:w-10 lg:h-10 animate-appearSpinSlight relative block w-8 h-8 focus:outline-none`,
+						]}
+					>
+						<div
+							css={[
+								tw`w-full h-full flex items-center justify-center pointer-events-none text-center`,
+								css`
+									-webkit-mask-image: -webkit-radial-gradient(
+										white,
+										black
+									);
+									transition: transform 0.5s
+										cubic-bezier(0.215, 0.61, 0.355, 1);
+									transform: rotate(0deg);
+									align-items: center;
+									-webkit-box-pack: center;
+								`,
+							]}
+						>
+							<h6
+								css={[
+									tw`font-theme`,
+									css`
+										font-size: 56px;
+										font-weight: 300;
+										line-height: 56px;
+										letter-spacing: -0.01em;
+									`,
+								]}
+							>
+								+
+							</h6>
+						</div>
+					</button>
+				</div>
+			</div>
+		</nav>
+	</header>
 )
 
 Header.propTypes = {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
+
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
 	purge: ["./src/**/*.{js,jsx,ts,tsx}"],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		colors: {
+			primaryGrey: "#f2f5f2",
+			home: "#172138",
+		},
+		fontFamily: {
+			theme: ['"ABC Monument Grotesk"', ...defaultTheme.fontFamily.sans],
+		},
+		extend: {
+			animation: {
+				appearSpinSlight: "appearSpinSlight 1s cubic-bezier(0.215, 0.61, 0.355, 1) forwards",
+			},
+			keyframes: {
+				appearSpinSlight: {
+					"0%": {
+						opacity: "0",
+						transform: "rotate(-180deg)",
+					},
+					"100%": {
+						opacity: "1",
+						transform: "rotate(0)",
+					},
+				},
+			},
+		},
 	},
 	variants: {
 		extend: {},
